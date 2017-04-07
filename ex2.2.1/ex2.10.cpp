1970b8793e9454ad030993f6a1f110f80110a3dc
unknown <>
2017-04-07 22:48:07 +0800

    after running and answer is added

--- ex2.2.1/ex2.10.cpp
+++ ex2.2.1/ex2.10.cpp
@@ -6,10 +6,13 @@ std::string global_str;
 int global_int;
 
 int main() {
-	int local_int;
+	int local_int=0;
 	std::string local_str;
 
-	std::cout<<global_str<<std::endl;
-	std::cout<<global_int<<std::endl;
+	std::cout<<global_str<<std::endl;//empty string
+	std::cout<<global_int<<std::endl;//zero
+
+	std::cout << local_str << std::endl;//uninitialized local string is a empty string
+	std::cout << local_int << std::endl;//uninitialized int is warned by compiler, the value is garbage
 	
 }
