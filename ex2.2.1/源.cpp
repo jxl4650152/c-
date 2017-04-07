2462854b3abe8f621dda0617556a1dd7ac152327
unknown <>
2017-04-07 22:04:35 +0800

    add some comment

--- ex2.2.1/源.cpp
+++ ex2.2.1/源.cpp
@@ -1,9 +1,9 @@
 #include<iostream>
 
 int main() {
-	std::cin >> int input_value;
-	int i = { 3.14 };
-	double salary = wage = 9999.99;
-	int j = 3.14;
+	std::cin >> int input_value; //invalid define.
+	int i = { 3.14 }; //list initialization doesn't permit this. 
+	double salary = wage = 9999.99;//undefined wage.
+	int j = 3.14;//get a warning, the value of j will be 3.
 
 }
